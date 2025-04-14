a7843248295faf30cd196e025d34bab5c74ea7cf
pmrowczynski11 <2282635@johnabbottcollege.net>
2025-04-14 09:18:22 -0400

    version3

--- footer.php
+++ footer.php
@@ -1,42 +1,35 @@
-<?php 
-    // START STEP 2.5 BUILD FOOTER
-    // Add this to all your php files for added security
+<?php
+if ( !defined( 'ABSPATH' ) )
+exit;
+?>
 
-        if (!defined('ABSPATH'))
-          exit; 
-             // Exit if accessed directly. 
-             // Ex: Not being called up directly by index.php or some other theme PHP file
-         ?>
+<footer>
 
-      <footer>
-         <!-- START STEP 2.21 ADD FOOTER MENUS -->
- <nav>
-     <?php wp_nav_menu( array( 
-         'theme_location' => 'footer-menu', 
-         'container_class' => 'footer-nav' 
-         ) ); ?>
- </nav>
+<nav>
+    <?php wp_nav_menu( array(
+        'theme_location' => 'footer-menu',
+        'container_class' => 'footer-nav'
+    ) );
+    ?>
+</nav>
 
- <nav>
-     <?php wp_nav_menu( array( 
-         'theme_location' => 'social-menu', 
-         'container_class' => 'footer-social' 
-         ) ); ?>
- </nav>
+<nav>
+    <?php wp_nav_menu( array(
+        'theme_location' => 'social-menu',
+        'container_class' => 'footer-social'
+    ) );
+    ?>
+</nav>
 
+<p class = 'copyright'>&copy;
+<?php echo bloginfo( 'name' ) ?>, <?php echo date( 'Y' );
+?></p>
 
- <p class="copyright">&copy; <?php echo bloginfo('name') ?>, <?php echo date("Y"); ?></p>
- <!-- END STEP 2.21 ADD FOOTER MENUS -->
-    
-      </footer>
+</footer>
 
-    <!-- START STEP 2.7 WP_FOOTER CONTENT -->
+<?php wp_footer();
+?>
 
+</body>
 
-    <?php wp_footer(); ?>
-        <?php // this functions loads the footer content ?>   
-
-
-    <!-- END STEP 2.7 WP_FOOTER CONTENT -->
-    </body>
-    </html><?php // END STEP 2.5 BUILD FOOTER ?>
+</html>
--- header.php
+++ header.php
@@ -1,82 +1,58 @@
-<?php  // START STEP 2.4
-    // Add this to all your php files for added security
-    if (!defined('ABSPATH'))
-        exit; 
-        // Exit if accessed directly. 
-        // Ex: Not being called up directly by index.php or some other theme PHP file
-    ?>
-    
-    <!-- START STEP 2.4 ADD HEADER.PHP CODE -->
-    <!DOCTYPE html>
-        <!-- START STEP 2.15 ADD HTML LANGUAGE ATTRIBUTES -->
-
-        <html <?php language_attributes(); ?>>
-
-<!-- END STEP 2.15 ADD HTML LANGUAGE ATTRIBUTES -->
-
-    <head>
-            <!-- START 2.12 WP_HEAD GET DYNAMIC HEADER CONTENT -->
-
+<?php
+if ( !defined( 'ABSPATH' ) )
+exit;
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <?php // add font code here ?>
     <?php wp_head(); ?>
-
-<!-- END 2.12 WP_HEAD GET DYNAMIC HEADER CONTENT -->
-       <!-- START STEP 2.16 ADD CHARACTER SET -->
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<!-- END STEP 2.16 ADD CHARACTER SET -->
-
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php // the viewport metatag is what makes a design responsive ?>
-        
-
-    </head>
-
-    <!-- START STEP 2.17 CUSTOMIZE BODY CLASS & OPEN -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<!-- END STEP 2.17 CUSTOMIZE BODY CLASS & OPEN -->
-            <!-- START STEP 2.13 ADD SEARCH FORM -->
+<div class='align-right'>
+   <button class='hide-text'>Menu</button>
+</div>
 
-    <?php get_search_form(); ?>
-
-<!-- END STEP 2.13 ADD SEARCH FORM -->       
-
-    <!-- START STEP 2.18 BRANDING & DESCRIPTION -->
 <header>
-    <!-- START STEP 2.25 ADD CUSTOM LOGO -->
-  <?php 
-    // Enables Wordpress custom logo
-    // See: https://developer.wordpress.org/reference%2Ffunctions%2Fadd_theme_support%2F/#custom-logo
-    
-        if ( function_exists( 'the_custom_logo' ) ) {
+<div class='branding'>
+    <?php
+    if ( function_exists( 'the_custom_logo' ) ) {
         the_custom_logo();
-        }
+    }
     ?>
-    <!-- END STEP 2.25 ADD CUSTOM LOGO -->
-    <div class="branding">
-        <a href="<?php echo esc_url(home_url()); ?>">
-        <?php bloginfo('name'); ?>
+
+    <div class="siteinfo">
+        <a href='<?php echo esc_url(home_url()); ?>'>
+        <?php bloginfo( 'name' );
+        ?>
         </a>
+       
+
+        <div class='description'>
+        <?php bloginfo( 'description' );
+        ?>
+        </div> 
     </div>
-    
-    <div class="description">
-            <?php bloginfo('description'); ?>
-            <?php // This is the WP site tagline, not the meta description tag ?>
-    </div>
-<!-- END STEP 2.18 BRANDING & DESCRIPTION -->
-        <nav>
-                <!-- START STEP 2.11 ADD THE MENU TO THE PAGE -->
-    <?php 
-        wp_nav_menu(array(
-            'menu_class'	  	=> 'primary-menu',  // adds: class="primary-menu"
-            'theme_location' 	=> 'header-menu',   // is controlled as the "header menu" in the backend
-            'menu_id'	    	=> 'menu',          // adds: id="menu"
-            'container'         => 'div',           // menu is inside a div
-        ));
-    ?>
-    <!-- END STEP 2.11 ADD THE MENU TO THE PAGE -->
-        </nav>
-    </header>
-    <!-- END STEP 2.4 ADD HEADER.PHP CODE -->
-    <?php  // END OF STEP 2.4 ?>
+</div>
+
+<?php get_search_form(); ?>
+</header>
+
+
+<nav class="primary-nav">
+<?php
+wp_nav_menu( array(
+'menu_class'	  	=> 'primary-menu',  // adds: class = 'primary-menu'
+'theme_location' 	=> 'header-menu',   // is controlled as the 'header menu' in the backend
+'menu_id'	    	=> 'menu',          // adds: id = 'menu'
+'container'         => 'div',           // menu is inside a div class="menu-header-container"
+) );
+?>
+</nav>
